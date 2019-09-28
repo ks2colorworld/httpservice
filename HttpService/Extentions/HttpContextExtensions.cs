1b9fd61769b636951fe5eb55e822b3345181e2bf
Pon Cheol Ku <bbon@bbon.kr>
2019-09-28 21:37:23 +0900

    응답 모델을 사용해서 요청 헤더의 Acept  에 따라 응답 컨텐트 타입을 결정합니다.

---
+++ HttpService/Extentions/HttpContextExtensions.cs
@@ -0,0 +1,47 @@
+﻿using HttpService.Models;
+using HttpService.Serializer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace HttpService
+{
+    public static class HttpContextExtensions
+    {
+        /// <summary>
+        /// <see cref="ResponseModel"/> 객체로 응답을 작성합니다.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static Task ExecuteResponseModelResult(this HttpContext context, ResponseModel model)
+        {
+            ISerializer serializer = null;
+            var contentType = context.Request.Headers["Accept"];
+            if ("text/xml" == contentType)
+            {
+                serializer = new XmlSerializer();
+            }
+            
+            if("application/json" == contentType)
+            {
+                serializer = new JsonSerializer();
+            }
+
+            if (serializer != null)
+            {
+                var content = serializer.Serialize(model);
+
+                context.Response.Headers["content-type"] = contentType;
+                return context.Response.WriteAsync(content);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
