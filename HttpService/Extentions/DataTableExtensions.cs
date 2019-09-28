e5bddff7c17bf2297108086b29ac50ce0eece69b
Pon Cheol Ku <bbon@bbon.kr>
2019-09-28 21:34:00 +0900

    Dynamic 객체는 직렬화, 역직렬화에 문제가 있어 사용하지 않습니다.

--- HttpService/Extentions/DataTableExtensions.cs
+++ HttpService/Extentions/DataTableExtensions.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Data;
 using System.Dynamic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace HttpService
 {
@@ -14,27 +18,82 @@ namespace HttpService
         /// </summary>
         /// <param name="table">데이터 테이블</param>
         /// <returns>다이나믹 객체 열거형</returns>
-        public static IEnumerable<dynamic> ToDynamicEnumerable(this DataTable table)
+        public static IEnumerable<DynamicRow> ToDynamicEnumerable(this DataTable table)
         {
             return table.AsEnumerable().Select(row => new DynamicRow(row));
+            
         }
 
-        private sealed class DynamicRow : DynamicObject
+        /// <summary>
+        /// 데이터 테이블을 사전 열거형으로 변환합니다.
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public static IEnumerable< Dictionary<string, object>> ToDictionaryEnumerable(this DataTable table)
         {
-            private readonly DataRow row;
+            return table.Select().Select(row => GetRows(row));
+        }
 
-            internal DynamicRow(DataRow row)
+        private static Dictionary<string, object> GetRows(DataRow row)
+        {
+            Dictionary<string, object> dictionary = new Dictionary<string, object>();
+
+            foreach (DataColumn col in row.Table.Columns)
             {
-                this.row = row;
+                dictionary.Add(col.ColumnName, row[col.ColumnName]);
             }
 
-            public override bool TryGetMember(GetMemberBinder binder, out object result)
-            {
-                var hasField = row.Table.Columns.Contains(binder.Name);
-                
-                result = hasField ? row[binder.Name] : null;
+            return dictionary;
+        }
 
-                return hasField;
+      
+    }
+
+    [DataContract]
+    public sealed class DynamicRow : DynamicObject, IXmlSerializable
+    {
+        private readonly DataRow row;
+
+        internal DynamicRow(DataRow row)
+        {
+            this.row = row;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            var hasField = row.Table.Columns.Contains(binder.Name);
+
+            result = hasField ? row[binder.Name] : null;
+
+            return hasField;
+        }
+
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            foreach (DataColumn col in row.Table.Columns)
+            {
+                yield return col.ColumnName;
+            }
+        }
+
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            var names = GetDynamicMemberNames();
+            foreach (var name in names)
+            {
+                writer.WriteStartElement(name);
+                writer.WriteValue(row[name]);
+                writer.WriteEndElement();
             }
         }
     }
